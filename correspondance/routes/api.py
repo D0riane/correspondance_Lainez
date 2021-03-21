de815e6ffd8ca2982d651a1286dc65454998505a
Doriane Hare <73248485+D0riane@users.noreply.github.com>
2021-03-21 13:23:08 +0100

    API
    API

--- correspondance/routes/api.py
+++ correspondance/routes/api.py
@@ -1,9 +1,10 @@
-from flask import render_template, request, url_for, jsonify
+from flask import request, url_for, jsonify
 from urllib.parse import urlencode
+from sqlalchemy import or_
 
 from ..app import app
 from ..constantes import LETTRES_PAR_PAGE, API_ROUTE
-from ..modeles.donnees import Lettre, Utilisateur, Publication, Transcription
+from ..modeles.donnees import Lettre, Publication, Transcription
 
 
 def Json_404():
@@ -14,15 +15,37 @@ def Json_404():
 
 @app.route(API_ROUTE+"/lettres")
 def api_lettres():
+    """
+    Récupérer les données de toutes les lettres en JSON
+    """
+    query = Lettre.query
+
     try:
-        query = Lettre.query.all()
-        return jsonify(query.to_jsonapi_dict())
-    except:
+        lettres = query
+
+    except Exception:
         return Json_404()
+
+    dict_lettres = {
+            "links": {
+                "self": request.url
+            },
+            "data": [
+                lettres.to_jsonapi_dict()
+                for lettres in lettres
+            ]
+        }
+
+    response = jsonify(dict_lettres)
+
+    return response
 
 
 @app.route(API_ROUTE+"/lettres/<lettre_id>")
 def api_lettre_unique(lettre_id):
+    """
+    Récupérer les données de la lettre en JSON
+    """
     try:
         query = Lettre.query.get(lettre_id)
         return jsonify(query.to_jsonapi_dict())
@@ -32,15 +55,35 @@ def api_lettre_unique(lettre_id):
 
 @app.route(API_ROUTE+"/publications")
 def api_publications():
+    """
+    Récupérer les données de toutes les publications en JSON
+    """
+    query = Publication.query
     try:
-        query = Publication.query.all()
-        return jsonify(query.to_jsonapi_dict())
-    except:
+        publications = query
+
+    except Exception:
         return Json_404()
+
+    dict_publications = {
+            "links": {
+                "self": request.url
+            },
+            "data": [
+                publications.to_jsonapi_dict()
+                for publications in publications
+            ]
+        }
+
+    response = jsonify(dict_publications)
+    return response
 
 
 @app.route(API_ROUTE+"/publications/<publication_id>")
 def api_publication_unique(publication_id):
+    """
+    Récupérer les données de la publication en JSON
+    """
     try:
         query = Publication.query.get(publication_id)
         return jsonify(query.to_jsonapi_dict())
@@ -50,15 +93,35 @@ def api_publication_unique(publication_id):
 
 @app.route(API_ROUTE+"/transcriptions")
 def api_transcriptions():
+    """
+    Récupérer les données de toutes les transcriptions en JSON
+    """
+    query = Transcription.query
     try:
-        query = Transcription.query.all()
-        return jsonify(query.to_jsonapi_dict())
-    except:
+        transcriptions = query
+
+    except Exception:
         return Json_404()
+
+    dict_transcriptions = {
+            "links": {
+                "self": request.url
+            },
+            "data": [
+                transcriptions.to_jsonapi_dict()
+                for transcriptions in transcriptions
+            ]
+        }
+
+    response = jsonify(dict_transcriptions)
+    return response
 
 
 @app.route(API_ROUTE+"/transcriptions/<transcription_id>")
 def api_transcription_unique(transcription_id):
+    """
+    Récupérer les données de la transcription en JSON
+    """
     try:
         query = Transcription.query.get(transcription_id)
         return jsonify(query.to_jsonapi_dict())
@@ -68,26 +131,25 @@ def api_transcription_unique(transcription_id):
 
 @app.route(API_ROUTE+"/recherche")
 def api_lettres_recherche():
-    """ Route permettant d'avoir le résultat d'une recherche en JSON
     """
-    # q est très souvent utilisé pour indiquer une capacité de recherche
-    motclef = request.args.get("q", None)
-    page = request.args.get("page", 1)
+    Route permettant d'avoir le résultat d'une recherche en JSON
+    """
+    # Récupération du mot clé renseigné par l'utilisateur.
+    motclef = request.args.get("keyword", None)
 
-    if isinstance(page, str) and page.isdigit():
-        page = int(page)
-    else:
-        page = 1
-
+    # Si il y a un mot clé, on filtre grâce à .like les résultats de la recherche.
     if motclef:
-        query = Lettre.query.filter(
-            Lettre.date_envoi.like("%{}%".format(motclef))
-        )
+        query = Lettre.query.filter(or_(Lettre.lettre_date.like("%{}%".format(motclef)),
+                                        Lettre.lettre_redacteur.like("%{}%".format(motclef)),
+                                        Lettre.lettre_lieu.like("%{}%".format(motclef)),
+                                        Lettre.lettre_volume.any(Publication.publication_titre.like("%{}%".format(
+                                            motclef)))))
     else:
         query = Lettre.query
 
     try:
-        resultats = query.paginate(page=page, per_page=LETTRES_PAR_PAGE)
+        resultats = query
+
     except Exception:
         return Json_404()
 
@@ -97,25 +159,10 @@ def api_lettres_recherche():
         },
         "data": [
             lettres.to_jsonapi_dict()
-            for lettres in resultats.items
+            for lettres in resultats
         ]
     }
 
-    if resultats.has_next:
-        arguments = {
-            "page": resultats.next_num
-        }
-        if motclef:
-            arguments["q"] = motclef
-        dict_resultats["links"]["next"] = url_for("api_lettres_recherche", _external=True)+"?"+urlencode(arguments)
-
-    if resultats.has_prev:
-        arguments = {
-            "page": resultats.prev_num
-        }
-        if motclef:
-            arguments["q"] = motclef
-        dict_resultats["links"]["prev"] = url_for("api_lettres_recherche", _external=True)+"?"+urlencode(arguments)
-
     response = jsonify(dict_resultats)
     return response
+
